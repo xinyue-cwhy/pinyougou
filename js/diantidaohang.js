1eca6e5a07695f6ca1ef51a5d8932b45c320ff11
xinyue-cwhy <2644965101@qq.com>
2021-04-02 17:11:02 +0800

    电梯导航

--- js/diantidaohang.js
+++ js/diantidaohang.js
@@ -1,20 +1,26 @@
 $(function () {
+  var flag = true
   var homeuseHeight = $('.homeuse').offset().top
   $(window).scroll(function () {
     floorhide();
-    $('.floor>div').each(function (i, n) {
-      if ($(document).scrollTop() >= $(n).offset().top) {
-        $('.fixedtool ul li').eq(i).addClass('current').siblings().removeClass('current')
-      }
-    })
+    if (flag) {
+      $('.floor>div').each(function (i, n) {
+        if ($(document).scrollTop() >= $(n).offset().top) {
+          $('.fixedtool ul li').eq(i).addClass('current').siblings().removeClass('current')
+        }
+      })
+    }
   })
   $('.fixedtool ul').on('click', 'li', function () {
+    flag = false
     $(this).addClass('current')
     $(this).siblings().removeClass('current')
     var index = $(this).index();
     var top = $('.floor>div').eq(index).offset().top
     $('html,body').stop().animate({
       scrollTop: top
+    }, function () {
+      flag = true
     })
   })
   floorhide();
